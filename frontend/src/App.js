25609c032003218fc20f17b873a9131ed358c61f
OshanBathiya <bathiya.oshan@gmail.com>
2022-10-12 22:25:06 +0530

    Doctor add and update UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import Header from './components/Header/Header';
 import AdminSignIn from './components/AdminManagement/AdminLogin';
 import Footer from './components/Footer/Footer';
+import AllDoctors from './components/DoctorList/alldoctors'
+import DoctorAdd from './components/DoctorAdd/DoctorAdd'
+import DoctorUpdate from './components/DoctorUpdate/DoctorUpdate'
 
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
             <Header/>
             
             <Route path="/admin/signin" exact component={AdminSignIn} />
+            <Route path="/doctor/all" exact component={AllDoctors} />
+            <Route path="/doctor/add" exact component={DoctorAdd} />
+            <Route path="/doctor/update/:id" exact component={DoctorUpdate} />
 
             <Footer/>
         </div>
