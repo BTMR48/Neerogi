a0d1026383d757a0122cd0a09b0a7525fd2eb497
OshanBathiya <bathiya.oshan@gmail.com>
2022-10-15 09:57:04 +0530

    user signup and login

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/', doctorRoutes.routes);
+app.use('/doctor', doctorRoutes.routes);
 app.use('/user', userRoutes.routes);
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PrivateRoute from './Routes/PrivateRoute';
 
 import './App.css';
 import Header from './components/Header/Header';
-import AdminSignIn from './components/AdminManagement/AdminLogin';
+import SignUp from './components/UserManagement/SignUp/SignUp';
+import Login from './components/UserManagement/SignIn/SignIn';
 import Footer from './components/Footer/Footer';
 import AllDoctors from './components/DoctorList/alldoctors'
 import DoctorAdd from './components/DoctorAdd/DoctorAdd'
 import DoctorUpdate from './components/DoctorUpdate/DoctorUpdate'
-
+import AllDoctorsClient from './components/DoctorCards/AllDoctors'
+import SingleDoctor from './components/DoctorCards/SingleDoctor'
 
 function App() {
   return (
@@ -16,10 +19,13 @@ function App() {
         <div>
             <Header/>
             
-            <Route path="/admin/signin" exact component={AdminSignIn} />
-            <Route path="/doctor/all" exact component={AllDoctors} />
-            <Route path="/doctor/add" exact component={DoctorAdd} />
-            <Route path="/doctor/update/:id" exact component={DoctorUpdate} />
+            <Route path="/user/signup" exact component={SignUp} />
+            <Route path="/user/signin" exact component={Login} />
+            <PrivateRoute path="/doctor/all" exact component={AllDoctors} />
+            <PrivateRoute path="/doctor/add" exact component={DoctorAdd} />
+            <PrivateRoute path="/doctor/update/:id" exact component={DoctorUpdate} />
+            <Route path="/doctor/cards" exact component={AllDoctorsClient} />
+            <Route path="/doctor/cards/:id" exact component={SingleDoctor} />
 
             <Footer/>
         </div>
